5149b382d36217c7a0de3f8f3748583b8857c4be
Nilesh Hadawale <nileshhadawale-oss@users.noreply.github.com>
2026-07-17 19:34:10 -0400

    uiui

--- tests/e2e_comprehensive.py
+++ tests/e2e_comprehensive.py
@@ -5352,7 +5352,7 @@ async def run_extended_scenarios(s: Suite, browser):
 
         sw_ok = (
 
-            "civicradar-v242" in sw_src
+            "civicradar-v243" in sw_src
 
             and "'/index.html'" not in sw_src
 
@@ -8501,7 +8501,7 @@ async def run_smoke_extended_tests(s: Suite, browser):
 
         sw_ok = (
 
-            "civicradar-v242" in sw_src
+            "civicradar-v243" in sw_src
 
             and "'/index.html'" not in sw_src
 
